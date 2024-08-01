6e032ef6daca7aa8a21927dddcec2bcc7bbdd059
Caio Keven <caiokeven3176@gmail.com>
2024-08-01 20:47:00 -0300

    -create url  completed

--- app/controllers/UrlBuilderController.php
+++ app/controllers/UrlBuilderController.php
@@ -3,19 +3,43 @@
 namespace app\controllers;
 
 use app\models\UrlBuilderModel;
+use Exception;
 
 class UrlBuilderController
 {
     public function home()
     {
-        
         require_once __DIR__ . '/../views/UrlBuilderView.php';
-        
     }
     public function CreateUrl()
     {
-        echo "entrouaaaaaaaaa aq";
-        
-    }
-}
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            try {
+                $utms = [];
+                $genered_url = " ";
+                $url = filter_var($_POST['url'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                $utms['utm_campaign'] = filter_var($_POST['utm_campaign'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                $utms['utm_source'] = filter_var($_POST['utm_source'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                $utms['utm_medium'] = filter_var($_POST['utm_medium'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                $utms['utm_content'] = filter_var($_POST['utm_content'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                $utms['utm_term'] = filter_var($_POST['utm_term'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                foreach ($utms as $index => $utm) {
+                    if ($genered_url == " ") {
+                        $genered_url = $url . "?" . $index . "=" . $utm;
+                    } else {
+                        $genered_url = $genered_url . "&" . $index . "=" . $utm;
+                    }
 
+                }
+                $new_url = new UrlBuilderModel($genered_url);
+                $new_url->saveUrl();    
+                echo "salvo com sucesso";
+                
+            } catch (Exception $e) {
+                echo "ocorreu o erro:" . $e->getMessage();
+            }
+
+        }
+    }
+
+}
